a565561efe71bc554e7301523973d8830d6442b4
cuongnm265 <cuongnm265@gmail.com>
2018-04-13 20:28:18 +0700

    Initial Commit

---
+++ nuxt.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'iportfolio',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Simple app for people to create and share their CV template in an elegant way' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
